a3983a3b9ee413fb140ffcc26eaa56d330e512ad
Sebas Medina1 <sebas22medi@gmail.com>
2024-10-19 20:48:29 -0300

    Agregar validador de usuario 4

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
-export const PORT = process.env.PORT || 3000
-export const DB_HOST = process.env.DB_HOST || 'localhost'
-export const DB_USER = process.env.DB_USER || 'prueba' 
-export const DB_PASSWORD= process.env.DB_PASSWORD || 'prueba123' 
-export const DB_NAME = process.env.DB_NAME || 'prueba'
-export const DB_PORT = process.env.DB_PORT || 3306
+// config.js
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    DB_HOST: process.env.DB_HOST || 'tus-credenciales-railway-host',  // El host de tu base de datos en Railway
+    DB_NAME: process.env.DB_NAME || 'tus-credenciales-railway-db-name',  // El nombre de la base de datos
+    DB_PASSWORD: process.env.DB_PASSWORD || 'tus-credenciales-railway-password',  // La contraseña proporcionada por Railway
+    DB_PORT: process.env.DB_PORT || 3306,  // El puerto proporcionado por Railway (generalmente 3306 para MySQL)
+    DB_USER: process.env.DB_USER || 'tus-credenciales-railway-user'  // El usuario de la base de datos
+};
